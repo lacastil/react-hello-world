dba642ef25f9fa9ae16b72a723fce1b545ff3e05
Luis Angel Castillo Ortiz <lacastil@Luiss-MacBook-Pro.local>
2021-07-21 13:16:35 -0500

    implementacion de VirtualDOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, {Component} from 'react';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import Counter from './components/counter/Counter';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Counter></Counter>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render (){
+    return (
+      <div className="LearningComponents">
+        My Hello World
+        <FirstComponent></FirstComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+      </div>
+    );
+  }
+}
+
+function Welcome() {
   return (
     <div className="App">
       <header className="App-header">
